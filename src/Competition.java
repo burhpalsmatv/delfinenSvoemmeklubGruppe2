462c6910cb0d467ee88747f65953809955cc384e
griffinflight <emma.saul2002@gmail.com>
2026-05-06 09:37:17 +0200

    a few classes (no constructors yet)

---
+++ src/Competition.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Competition {
+    private ArrayList<SwimmingCategory> swimmingCategories;
+    private ArrayList<Competitor> competitors;
+    public String date;
+    public String title;
+    public String prize;
+    public boolean ended;
+}
---
+++ src/Trainer.java
@@ -0,0 +1,12 @@
+public class Trainer {
+    private String name;
+    private int age;
+    private String adress;
+    private String postalcode;
+    private String phone;
+    private String email;
+    private Gender gender;
+    private SwimmingCategory swimmingCategory;
+
+    
+}
