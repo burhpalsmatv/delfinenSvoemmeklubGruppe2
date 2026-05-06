3fb8148f6e3b6ccd4304dcf626d7ea4d94d6871f
griffinflight <emma.saul2002@gmail.com>
2026-05-06 10:30:44 +0200

    A few things

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,12 +1,22 @@
 public class Trainer {
     private String name;
     private int age;
-    private String adress;
-    private String postalcode;
+    private String address;
+    private String postalCode;
     private String phone;
     private String email;
     private Gender gender;
     private SwimmingCategory swimmingCategory;
 
-    
+
+    public Trainer(String name, int age, String address, String postalCode, String phone, String email,
+                   Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.email = email;
+        this.gender = gender;
+    }
 }
