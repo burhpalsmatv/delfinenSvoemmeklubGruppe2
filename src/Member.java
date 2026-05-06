e289a9f5a9cb7e3d9206f5ce9fe2849d1f463a8c
burhpalsmatv <hogstedm@gmail.com>
2026-05-06 09:38:05 +0200

    task_1.1

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,23 @@
 public class Member {
+    private String name;
+    private int age;
+    private String address;
+    private String postcode;
+    private String phone;
+    private String email;
+    private Gender gender;
+    private boolean betalingsStatus;
+    private MedlemsType medlemstype;
+    private boolean seniorRabat;
+
+    public Member(String name, MedlemsType medlemsType, String phone){
+        this.name = name;
+        this.medlemstype = medlemsType;
+        this.phone = phone;
+    }
+
+    public Member (){
+
+    }
+
 }
