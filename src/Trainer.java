52a8744e8e103a8d527b633396ab9fd4b75d12dd
griffinflight <emma.saul2002@gmail.com>
2026-05-07 09:16:09 +0200

    2.3 Quotagetter

--- src/Trainer.java
+++ src/Trainer.java
@@ -148,7 +148,8 @@ public class Trainer {
     return text;
     }
 
+    /*
     public void setSwimmingCategory(SwimmingCategory swimmingCategory) {
         this.swimmingCategory = swimmingCategory;
-    }
+    }*/
 }
