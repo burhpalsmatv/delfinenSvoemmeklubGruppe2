81f855d2953b183c2eb1ba99905892ceaa24d209
griffinflight <emma.saul2002@gmail.com>
2026-05-08 10:35:26 +0200

    UI update (Trainer screen)

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
 
-        ArrayList<Member> theGang = new ArrayList<Member>();
-        Member mads = new Member("Mads", Membership.SENIOR, "22334455");
-        Member emma = new Member("Emma", Membership.JUNIOR, "99999999");
-        Member mattie = new Member("Mattie", Membership.PASSIVE, "80081350");
-        Member valdemar = new Member("Valdemar", Membership.JUNIOR, "00000001");
-
-        theGang.add(mads);
-        theGang.add(emma);
-        theGang.add(mattie);
-        theGang.add(valdemar);
-
-        SwimmingCategory swimtest1 = new SwimmingCategory("Lars Henrik");
-        Trainer tester1 = new Trainer("Poul", swimtest1);
-
-        Member.setNameAt(0, "Morten");
-        Member.removeMemberAt(0);
-        Trainer.deleteTrainer(0);
-
-        System.out.println(Member.listOfMembers);
-        PricingManager.changeMembershipPrice(MemberType.JUNIOR, 1200);
-
-        Competitor comptest1 = new Competitor(emma);
-        Competition stævne1 = new Competition("Title1", "idag", comptest1);
-
-        System.out.println(comptest1);
-        System.out.println(stævne1);
-
-
-        mads.setIsPaid();
+        TrainerScreen.trainerScreen();
     }
 }
--- src/TrainerScreen.java
+++ src/TrainerScreen.java
@@ -3,13 +3,82 @@ public class TrainerScreen {
 
 
     public static void trainerScreen() {
-        System.out.println("-----------------------------");
-        System.out.println("Træner menu");
-        System.out.println("1. Stævner");
-        System.out.println("2. Vis liste af konkurrencesvømmere");
-        System.out.println("3. Tilføj disciplin til svømmer");
-        System.out.println("4. Tilføj svømmer til stævne");
-        System.out.println("5. Træningsresultater");
-        System.out.println("6. Stævneresultater");
-        System.out.println("0. Log ud");    }
+
+        boolean trainerLoggedIn = true;
+
+        while (trainerLoggedIn) {
+            System.out.println(trainerScreenString());
+
+            int input = Application.scanner.nextInt();
+
+            switch(input) {
+
+                //COMPETITIONS:
+                case 1: competitionMenu();
+
+
+                //LIST OF COMPETITORS
+                case 2:
+
+
+                //ADD CATEGORY TO SWIMMER
+                case 3:
+
+
+                //ADD SWIMMER TO COMPETITION
+                case 4:
+
+
+                // TRAINING RESULTS
+                case 5:
+
+
+                //COMPETITION RESULTS
+                case 6:
+
+
+                //LOG OUT
+                case 0:
+                trainerLoggedIn = false;
+            }
+
+
+
+
+        }
+
+    }
+
+
+    public static void competitionMenu() {
+        System.out.println(comptetitionMenuString());
+
+    }
+
+
+//RANDOM ASS STRINGS:
+
+    public static String trainerScreenString(){
+        return String.format("""
+                "-----------------------------"
+                "Træner menu"
+                1. Stævner
+                2. Vis liste af konkurrencesvømmere
+                3. Tilføj disciplin til svømmer
+                4. Tilføj svømmer til stævne
+                5. Træningsresultater
+                6. Stævneresultater
+                0. Log ud
+                """);
+    }
+
+    public static String comptetitionMenuString(){
+        return String.format("""
+                STÆVNER
+                1. Vis kommende
+                2. Vis tidligere
+                0. Tilbage til menu
+                """);
+    }
+
 }
