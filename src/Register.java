f4bf3dfbd08ac90e829577fa8ea28c653a8bf09f
valdemar <duedamvaldemar@gmail.com>
2026-05-08 10:31:14 +0200

    register lavet

---
+++ src/Register.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Register {
+    static ArrayList<Member> listOfMembers = new ArrayList<>();
+    static ArrayList<Trainer> listOfTrainers = new ArrayList<>();
+    static ArrayList<Competitor> listOfCompetitors = new ArrayList<>();
+    static ArrayList<Competition> listOfCompetitions = new ArrayList<>();
+
+    public Register () {}
+
+
+
+}
