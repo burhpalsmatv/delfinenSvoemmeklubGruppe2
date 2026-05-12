5a587c35b303705da040c0c85e4200205d7be927
aliza <alizaakhtar06@icloud.com>
2026-05-12 10:05:17 +0200

    switch case lavet for case 1 og 2 i metoden pricingMenu i TreasurerScreen klassen

--- src/TreasurerScreen.java
+++ src/TreasurerScreen.java
@@ -53,22 +53,47 @@ public class TreasurerScreen {
             switch (priceInput) {
 
                 //CURRENT PRICES
-                case 1: //INSERT METHOD
+                case 1:
+                    showCurrentPrices();
 
-                    //CHANGE PRICES
-                case 2: //INSERT METHOD
+                //CHANGE PRICES
+                case 2:
+                    changePrices();
 
-                    //CHANGE DISCOUNT
+                //CHANGE DISCOUNT
                 case 3: //INSERT METHOD
 
-                    //LOG OUT
+                //LOG OUT
                 case 0:
                     inPricingMenu = false;
+                }
             }
         }
     }
 
-    public static void quotaMenu() {
+
+
+private static void showCurrentPrices() {
+    System.out.println("---------DE NUVÆRENDE PRISER---------");
+    System.out.println(PricingManager.displayPrices());
+    System.out.println("Indtast \"0\" for at vende tilbage til hjem");
+}
+
+private static void changePrices() {
+    System.out.println("Indtast et tal (1:JUNIOR, 2:SENIOR, 3:PASSIVE):");
+    int choosenMembership = Application.scanner.nextInt();
+    Membership membership = Membership.values()[choosenMembership - 1];
+
+    System.out.println("Indtast ny pris: ");
+    int newPrice = Application.scanner.nextInt();
+
+    membership.setPrice(newPrice);
+
+    System.out.println("Prisen er nu ændret");
+    System.out.println("Indtast \"0\" for at vende tilbage til hjem");
+}
+
+public static void quotaMenu() {
         boolean inQuotaMenu = true;
         System.out.println(quotaMenuString());
 
