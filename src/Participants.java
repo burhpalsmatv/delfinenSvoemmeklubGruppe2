468ecf8b46d9949211c7b0d7f30a95b3d2061e83
burhpalsmatv <hogstedm@gmail.com>
2026-05-12 12:15:07 +0200

    participants klasse

---
+++ src/Participants.java
@@ -0,0 +1,11 @@
+public class Participants {
+    private String time;
+
+    public Participants(String time){
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
