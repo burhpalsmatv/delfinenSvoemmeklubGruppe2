cff48aa7996086d3456afe96035cff7d7d1db238
aliza <alizaakhtar06@icloud.com>
2026-05-15 13:13:36 +0200

    mikrofix

--- src/AM_MemberScreen.java
+++ src/AM_MemberScreen.java
@@ -103,7 +103,7 @@ public class AM_MemberScreen {
 
         while (true) {
             if (Register.listOfMembers.isEmpty()) {
-                System.out.println("Listen er tom\n");
+                System.out.println("Der er ingen medlemmer at redigere lige nu\n");
                 return;
 
             } else {
