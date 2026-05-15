eccd9e312dfa2cb78b17eecee9d47b223eabb639
valdemar <duedamvaldemar@gmail.com>
2026-05-15 11:05:33 +0200

    glue app _ 1

--- src/Register.java
+++ src/Register.java
@@ -1,10 +1,8 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-    // This is our database
-
-    // Main files
 public abstract class Register {
+
     // Member lists
     static ArrayList<Member> listOfMembers = new ArrayList<>(); // Alle slags members
     static ArrayList<MemberPassive> listOfPassiveMembers = new ArrayList<>();
